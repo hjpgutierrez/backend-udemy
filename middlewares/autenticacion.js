f6aee8a40387edf08b1711d6e7e1ccec88cc0eda
hjpgutierrez <hpaezgutierrez@gmail.com>
2019-01-05 19:10:58 -0500

    Sección 17 terminada

--- middlewares/autenticacion.js
+++ middlewares/autenticacion.js
@@ -28,3 +28,47 @@ exports.verificaToken = function(req, res, next) {
     });
 
 }
+
+
+
+// ==========================================
+//  Verificar Si es un usuario administrador
+// ==========================================
+exports.verificaADMIN_ROLE = function(req, res, next) {
+
+    var usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+        return;
+    } else {
+        return res.status(401).json({
+            ok: false,
+            mensaje: 'Token incorrecto',
+            errors: { message: 'No es administrador, no tienes accesos a esta función.' }
+        });
+    }
+
+}
+
+
+// ==========================================
+//  Verificar Si es un usuario administrador  o es el mismo usuario
+// ==========================================
+exports.verificaADMIN_ROLE_o_MismoUsuario = function(req, res, next) {
+
+    var usuario = req.usuario;
+    var id = req.params.id;
+
+    if (usuario.role === 'ADMIN_ROLE' || usuario._id === id) {
+        next();
+        return;
+    } else {
+        return res.status(401).json({
+            ok: false,
+            mensaje: 'Token incorrecto',
+            errors: { message: 'No es administrador, no eres tu mismo.' }
+        });
+    }
+
+}
